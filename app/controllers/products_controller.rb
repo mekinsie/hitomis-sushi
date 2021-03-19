aad251aca91848783851a1ff4e2ac784d0c5e21c
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-19 12:51:28 -0700

    Add product controller.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,54 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+    render :index
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    render :show
+  end
+
+  def new
+    @product = Product.new
+    render :new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Product successfully created"
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = "Product successfully updated"
+      redirect_to product_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:notice] = "Product successfully deleted"
+    redirect_to products_path
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :cost, :country_of_origin)
+  end
+end
