9e0f6f342578f7a0c47ff45ecaa730d29bfad4e1
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-19 12:31:13 -0700

    Write specs for reviews. Pass.

---
+++ spec/models/review_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Review do
+  it { should belong_to(:product) }
+  it { should validate_presence_of :author }
+  it { should validate_presence_of :content_body }
+  it { should validate_presence_of :rating }
+  it { should validate_numericality_of :rating }
+
+  # it("titleizes the author of the review") do
+  #   review = Review.create({:author => "mekinsie", :content_body => "The oatmeal tastes delicious.", :rating => 5})
+  #   expect(review.author).to eq("Mekinsie")
+  # end
+
+end
