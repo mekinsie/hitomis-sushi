5c6436474b574a3052e7092b56e7d4f97bc9296d
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-19 11:21:26 -0700

    Create models with CRUD functionality and validations.

---
+++ app/models/product.rb
@@ -0,0 +1,13 @@
+class Product < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :cost, presence: true, numericality: {  only_integer: true }
+  validates :country_of_origin, presence: true
+  before_save(:titleize_product)
+
+
+  private
+    def titleize_product
+      self.name = self.name.titleize
+    end
+end
---
+++ app/models/review.rb
@@ -0,0 +1,12 @@
+class Review < ApplicationRecord
+  belongs_to :product
+  validates :author, presence: true
+  validates :content_body, presence: true, length: {in: 50...250}
+  validates :rating, presence: true, numericality: {  only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  before_save(:titleize_author)
+
+  private
+    def titleize_author
+      self.author = self.author.titleize
+    end
+end
