08a7747be5f28e1a6c6a03f816c34fa25ddab159
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-19 12:56:15 -0700

    Add reviews controller.

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,55 @@
+class ReviewsController < ApplicationController
+
+  def show
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :show
+  end
+
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+    render :new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      flash[:notice] = "review successfully created"
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      flash[:notice] = "review successfully updated"
+      redirect_to review_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    @review.destroy
+    flash[:notice] = "review successfully deleted"
+    redirect_to product_path(@product)
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:author, :content_body, :rating)
+  end
+end
