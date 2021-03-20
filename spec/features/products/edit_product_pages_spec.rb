a21764501ddd69aae71a080d79da338f89fb0efc
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-19 17:47:07 -0700

    Add integration spec for editing a product.

---
+++ spec/features/products/edit_product_pages_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "the edit product process" do
+  it "edits name, cost, and counrtry of a product" do
+    Product.create({name: "Cheese Puffs", cost: 5.99, country_of_origin: "USA"})
+    visit '/products'
+    click_link 'Cheese Puffs'
+    click_link 'Edit Product'
+    fill_in "product_name", :with => 'cheeze puffz'
+    fill_in "product_cost", :with => '4.99'
+    fill_in "product_country_of_origin", :with => 'Mexico'
+    click_on 'Submit'
+    expect(page).to have_content "Cheeze Puffz"
+    expect(page).to have_content "4.99"
+    expect(page).to have_content "Mexico"
+  end
+end
