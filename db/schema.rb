dfb751708b0356729ed18d34c6878ccfc91417c8
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-26 12:42:28 -0700

    Add ability for users to edit their own review.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_170520) do
+ActiveRecord::Schema.define(version: 2021_03_26_190243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_03_26_170520) do
     t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   create_table "users", force: :cascade do |t|
