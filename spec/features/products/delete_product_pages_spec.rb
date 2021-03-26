4a4278d9a2b4eecce75f1e55a66b491e838a68f2
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-26 16:20:51 -0700

    Add integration for deleting with admin.

--- spec/features/products/delete_product_pages_spec.rb
+++ spec/features/products/delete_product_pages_spec.rb
@@ -1,11 +1,17 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe "the delete product process" do
-#   it "deletes a product" do
-#     Product.create({name: "Cheese Puffs", cost: 5.99, country_of_origin: "USA"})
-#     visit '/products'
-#     click_link 'Cheese Puffs'
-#     click_link 'Delete Product'
-#     expect(page).to have_no_content "Cheese Puffs"
-#   end
-# end
+describe "the delete product process" do
+  it "deletes a product" do
+    Product.create({name: "Cheese Puffs", cost: 5.99, country_of_origin: "USA"})
+    visit '/'
+    click_link 'Sign in'
+    fill_in 'user_email', :with => "admin@gmail.com"
+    fill_in 'user_password', :with => "adminpassword"
+    click_on 'Log in'
+    visit '/products/new'
+    visit '/products'
+    click_link 'Cheese Puffs'
+    click_link 'Delete Product'
+    expect(page).to have_no_content "Cheese Puffs"
+  end
+end
