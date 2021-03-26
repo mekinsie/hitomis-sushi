7ffd73e2d4cce5aee542a3e293e9a5b149b3bad6
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-26 16:23:46 -0700

    Add admin login for edit product.

--- spec/features/products/edit_product_pages_spec.rb
+++ spec/features/products/edit_product_pages_spec.rb
@@ -1,18 +1,22 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe "the edit product process" do
-#   it "edits name, cost, and counrtry of a product" do
-#     Product.create({name: "Cheese Puffs", cost: 5.99, country_of_origin: "USA"})
-#     current_user = User.create({email: "admin@gmail.com", password: "adminpassword", admin: true})
-#     visit '/products'
-#     click_link 'Cheese Puffs'
-#     click_link 'Edit Product'
-#     fill_in "product_name", :with => 'cheeze puffz'
-#     fill_in "product_cost", :with => '4.99'
-#     fill_in "product_country_of_origin", :with => 'Mexico'
-#     click_on 'Submit'
-#     expect(page).to have_content "Cheeze Puffz"
-#     expect(page).to have_content "4.99"
-#     expect(page).to have_content "Mexico"
-#   end
-# end
+describe "the edit product process" do
+  it "edits name, cost, and counrtry of a product" do
+    Product.create({name: "Cheese Puffs", cost: 5.99, country_of_origin: "USA"})
+    visit '/'
+    click_link 'Sign in'
+    fill_in 'user_email', :with => "admin@gmail.com"
+    fill_in 'user_password', :with => "adminpassword"
+    click_on 'Log in'
+    click_on 'Products'
+    click_link 'Cheese Puffs'
+    click_link 'Edit Product'
+    fill_in "product_name", :with => 'cheeze puffz'
+    fill_in "product_cost", :with => '4.99'
+    fill_in "product_country_of_origin", :with => 'Mexico'
+    click_on 'Submit'
+    expect(page).to have_content "Cheeze Puffz"
+    expect(page).to have_content "4.99"
+    expect(page).to have_content "Mexico"
+  end
+end
