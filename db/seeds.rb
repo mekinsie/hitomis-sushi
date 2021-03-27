7651751a02171fe981b0081f6aaa3802ae9f4315
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-26 22:29:54 -0700

    Edit faker seeds to be sushi.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@ Review.destroy_all
 User.destroy_all
 
 50.times do
-  product = Product.create!(name: Faker::Food.dish, cost: Faker::Number.decimal(l_digits: 2), country_of_origin: Faker::Address.country)
+  product = Product.create!(name: Faker::Food.sushi, cost: Faker::Number.decimal(l_digits: 2), country_of_origin: Faker::Address.country)
   5.times do
-    review = product.reviews.create!(author: Faker::FunnyName.name, content_body: Faker::Lorem.paragraph(sentence_count: 5), rating: Faker::Number.between(from:1, to: 5))
+    review = product.reviews.create!(author: Faker::FunnyName.name, content_body: Faker::Lorem.paragraph(sentence_count: 5), rating: Faker::Number.between(from:1, to: 5), email: Faker::Internet.email)
     product.reviews <<  review
   end
 end
